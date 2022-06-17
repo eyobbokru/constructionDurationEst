a245f324d94edd302f11af8c39aebc731dc9cef1
eyob70 <eyob.bokru@gmail.com>
2022-06-17 23:13:11 +0300

    spatia

---
+++ resources/js/Pages/Admin/Index.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <AdminLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <Welcome />
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+    import AdminLayout from '@/Layouts/AdminLayout.vue';
+     import Welcome from '@/Jetstream/Welcome.vue';
+  </script>
